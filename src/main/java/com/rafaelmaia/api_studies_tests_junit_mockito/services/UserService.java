fc91e66a759e4c4bd8ef1eabe4f42ef538ca4556
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-17 11:28:36 -0300

    create endpoint findAll in UserResource

--- src/main/java/com/rafaelmaia/api_studies_tests_junit_mockito/services/UserService.java
+++ src/main/java/com/rafaelmaia/api_studies_tests_junit_mockito/services/UserService.java
@@ -2,7 +2,10 @@ package com.rafaelmaia.api_studies_tests_junit_mockito.services;
 
 import com.rafaelmaia.api_studies_tests_junit_mockito.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
--- src/main/java/com/rafaelmaia/api_studies_tests_junit_mockito/services/impl/UserServiceImpl.java
+++ src/main/java/com/rafaelmaia/api_studies_tests_junit_mockito/services/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.rafaelmaia.api_studies_tests_junit_mockito.services.exceptions.Object
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,9 @@ public class UserServiceImpl implements UserService {
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("User not found!"));
+    }
+
+    public List<User> findAll() {
+        return repository.findAll();
     }
 }
