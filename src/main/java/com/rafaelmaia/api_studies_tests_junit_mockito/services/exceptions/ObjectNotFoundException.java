151f10c6d2d7fba0885e68d3acd14fbf89bb6705
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-17 11:03:20 -0300

    exception handling for User not found

---
+++ src/main/java/com/rafaelmaia/api_studies_tests_junit_mockito/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.rafaelmaia.api_studies_tests_junit_mockito.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/rafaelmaia/api_studies_tests_junit_mockito/services/impl/UserServiceImpl.java
+++ src/main/java/com/rafaelmaia/api_studies_tests_junit_mockito/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.rafaelmaia.api_studies_tests_junit_mockito.services.impl;
 import com.rafaelmaia.api_studies_tests_junit_mockito.domain.User;
 import com.rafaelmaia.api_studies_tests_junit_mockito.repositories.UserRepository;
 import com.rafaelmaia.api_studies_tests_junit_mockito.services.UserService;
+import com.rafaelmaia.api_studies_tests_junit_mockito.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("User not found!"));
     }
 }
