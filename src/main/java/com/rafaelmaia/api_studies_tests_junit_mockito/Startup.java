cc4be77f8000f76c3d049d41f7f6edb97cb20f51
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-17 06:43:06 -0300

    create project

---
+++ src/main/java/com/rafaelmaia/api_studies_tests_junit_mockito/Startup.java
@@ -0,0 +1,13 @@
+package com.rafaelmaia.api_studies_tests_junit_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelmaia/api_studies_tests_junit_mockito/ApiStudiesTestsJunitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelmaia.api_studies_tests_junit_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStudiesTestsJunitMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
