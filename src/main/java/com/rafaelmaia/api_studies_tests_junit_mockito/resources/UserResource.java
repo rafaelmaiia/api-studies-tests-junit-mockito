845c45fac3035816280809f8b8a6f6f4ccf316f3
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-17 09:40:32 -0300

    refactor method findById in UserResource

--- src/main/java/com/rafaelmaia/api_studies_tests_junit_mockito/resources/UserResource.java
+++ src/main/java/com/rafaelmaia/api_studies_tests_junit_mockito/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.rafaelmaia.api_studies_tests_junit_mockito.resources;
 
 import com.rafaelmaia.api_studies_tests_junit_mockito.domain.User;
+import com.rafaelmaia.api_studies_tests_junit_mockito.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
 
-        return ResponseEntity.ok().body(new User(1, "rafael", "rafael@gmail.com", "rafael123'"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
