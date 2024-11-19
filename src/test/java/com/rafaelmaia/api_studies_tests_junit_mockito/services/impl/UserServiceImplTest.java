07b563b125aca9b16a341910b916683ea7e0cc9b
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-19 15:03:09 -0300

    test to class UserService to method delete with exception

--- src/test/java/com/rafaelmaia/api_studies_tests_junit_mockito/services/impl/UserServiceImplTest.java
+++ src/test/java/com/rafaelmaia/api_studies_tests_junit_mockito/services/impl/UserServiceImplTest.java
@@ -154,6 +154,17 @@ class UserServiceImplTest {
         verify(repository, times(1)).deleteById(anyInt());
     }
 
+    @Test
+    void deleteWithObjectNotFoundException() {
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(USER_NOT_FOUND));
+        try {
+            service.delete(ID);
+        } catch (Exception ex) {
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals(USER_NOT_FOUND, ex.getMessage());
+        }
+    }
+
     private void startUser() {
         user = new User(ID, NAME, EMAIL, PASSWORD);
         userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
