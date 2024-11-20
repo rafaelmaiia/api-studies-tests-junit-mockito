62160b293302383a291ced706fc9c3a424d239de
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-20 11:12:21 -0300

    removing unused annotations

--- src/main/java/com/rafaelmaia/api_studies_tests_junit_mockito/resources/exceptions/StandardError.java
+++ src/main/java/com/rafaelmaia/api_studies_tests_junit_mockito/resources/exceptions/StandardError.java
@@ -2,12 +2,10 @@ package com.rafaelmaia.api_studies_tests_junit_mockito.resources.exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class StandardError {
 
