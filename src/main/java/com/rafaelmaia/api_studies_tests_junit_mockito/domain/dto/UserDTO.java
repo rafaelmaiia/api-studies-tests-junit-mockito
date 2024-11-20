ebd6e3a52cbe4593b4edf35c5b6d830892c7104b
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-20 11:17:15 -0300

    fixed bug

--- src/main/java/com/rafaelmaia/api_studies_tests_junit_mockito/domain/dto/UserDTO.java
+++ src/main/java/com/rafaelmaia/api_studies_tests_junit_mockito/domain/dto/UserDTO.java
@@ -3,10 +3,12 @@ package com.rafaelmaia.api_studies_tests_junit_mockito.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
 
     private Integer id;
